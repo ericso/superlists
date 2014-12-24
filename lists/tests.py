206f028de7f3df749347b44d20998777c81953ba
Eric So <me@eric.so>
2014-12-23 20:54:26 -0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -12,12 +13,23 @@ class HomePageTest(TestCase):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
 
+  # def test_home_page_returns_correct_html(self):
+  #   request = HttpRequest()
+  #   response = home_page(request)
+
+  #   print(repr(response.content))
+
+  #   # content from the response is in raw bytes
+  #   # we have to compare them with the b'' syntax
+  #   self.assertTrue(response.content.startswith(b'<html>'))
+  #   self.assertIn(b'<title>To-Do lists</title>', response.content)
+  #   self.assertTrue(response.content.strip().endswith(b'</html>'))
+
   def test_home_page_returns_correct_html(self):
     request = HttpRequest()
     response = home_page(request)
+    expected_html = render_to_string('home.html')
 
-    # content from the response is in raw bytes
-    # we have to compare them with the b'' syntax
-    self.assertTrue(response.content.startswith(b'<html>'))
-    self.assertIn(b'<title>To-Do lists</title>', response.content)
-    self.assertTrue(response.content.endswith(b'</html>'))
+    # .decode() converts the response.content bytes to a
+    #  Python Unicode string
+    self.assertEqual(response.content.decode(), expected_html)
