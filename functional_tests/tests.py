aaf1354906f14897d847381146dd9b92a70c7e8e
Eric So <me@eric.so>
2014-12-26 14:24:58 -0800

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,7 @@ from django.test import LiveServerTestCase
 
 class NewVisitorTest(LiveServerTestCase):
 
+  ### Non-testing methods ###
   def setUp(self):
     self.setUpBrowser()
 
@@ -20,16 +21,15 @@ class NewVisitorTest(LiveServerTestCase):
   def tearDownBrowser(self):
     self.browser.quit()
 
-
   def check_for_row_in_list_table(self, row_text):
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
     self.assertIn(row_text, [row.text for row in rows])
 
-
+  ### Testing Methods ###
   def test_can_start_a_list_and_retrieve_it_later(self):
-    # Edith has heard about a cool new online to-do app. She goes
-    # to check out its homepage
+    # Edith has heard about a cool new online to-do app.
+    # She goes to check out its homepage
     self.browser.get(self.live_server_url)
 
     # She notices the page title and header mention to-do lists
@@ -96,3 +96,26 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertIn("Buy milk", page_text)
 
     # Satisfied, she goes back to sleep
+
+  def test_layout_and_styling(self):
+    # Edith goes to the home page
+    self.browser.get(self.live_server_url)
+    self.browser.set_window_size(1024, 768)
+
+    # She notices the input box is nicely centered
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width']/2,
+      512,
+      delta=5
+    )
+
+    # She starts a new list and sees the input is nicely
+    #  centered there too
+    inputbox.send_keys('testing\n')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width']/2,
+      512,
+      delta=5
+    )
