f84a49319911786b1122015ea0f71f800f76fe92
Eric So <me@eric.so>
2015-01-08 21:40:11 -0800

    Validation of form using valudate_unique on both form and model

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,6 @@
 """Unit tests for views in lists app in superlists project"""
+from unittest import skip
+
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
@@ -100,6 +102,19 @@ class ListViewTest(TestCase):
     response = self.post_invalid_input()
     self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
+  @skip
+  def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+    list1 = List.objects.create()
+    item1 = Item.objects.create(list=list1, text='textey')
+    response = self.client.post(
+      '/lists/%d/' % (list1.id,),
+      data={'text': 'textey'}
+    )
+    expected_error = escape("You've already got this in your list")
+    self.assertContains(response, expected_error)
+    self.assertTemplateUsed(response, 'list.html')
+    self.assertEqual(Item.objects.all().count(), 1)
+
   def test_displays_item_form(self):
     list_ = List.objects.create()
     response = self.client.get('/lists/%d/' % (list_.id,))
@@ -124,13 +139,6 @@ class NewListTest(TestCase):
     )
     new_list = List.objects.first()
     self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
-
-  # def test_validation_errors_are_sent_back_to_home_page_template(self):
-  #   response = self.client.post('/lists/new', data={'text': ''})
-  #   self.assertEqual(response.status_code, 200)
-  #   self.assertTemplateUsed(response, 'home.html')
-  #   expected_error = escape("You can't have an empty list item")
-  #   self.assertContains(response, expected_error)
 
   def test_for_invalid_input_renders_home_page_template(self):
     response = self.client.post('/lists/new', data={'text': ''})
