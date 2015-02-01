87d2334737d79f60d8783ceca27be7f24d7df60a
Eric So <me@eric.so>
2015-01-31 19:15:26 -0800

    Custom Persona auth backend + custom user model

---
+++ accounts/static/accounts.js
@@ -0,0 +1,25 @@
+
+var initialize = function(navigator, user, token, urls) {
+
+  navigator.id.watch({
+    loggedInUser: user,
+    onlogin: function(assertion) {
+      $.post(
+        urls.login,
+        { assertion: assertion, csrfmiddlewaretoken: token }
+      ).done(function() { window.location.reload(); }
+      ).fail(function() { navigator.id.logout(); });
+    },
+    onlogout: function() {}
+  });
+
+  $('#id_login').on('click', function() {
+    navigator.id.request();
+  });
+};
+
+window.Superlists = {
+  Accounts: {
+    initialize: initialize
+  }
+};
