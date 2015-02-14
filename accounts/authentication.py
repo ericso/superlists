cbc8238f36b1ce37ddf46deb88445e6ef426d49c
Eric So <me@eric.so>
2015-02-14 14:32:18 -0800

    Logging to console

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,20 +1,29 @@
+import logging
 import requests
+
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
-DOMAIN = 'localhost'
+# DOMAIN = 'localhost'
+DOMAIN = 'superlists-staging.ericso.me'
+
 
 class PersonaAuthenticationBackend(object):
 
   def authenticate(self, assertion):
+    logging.warning("entering authenticate function")
+
     data = {
       'assertion': assertion,
       'audience': DOMAIN
     }
     response = requests.post(PERSONA_VERIFY_URL, data=data)
+
+    logging.warning("got response from persona")
+    logging.warning(response.content.decode())
 
     if response.ok and response.json()['status'] == 'okay':
       email = response.json()['email']
