80accf405b7a2370712ccc2d9c6e9e4c5028894f
Eric So <me@eric.so>
2015-02-14 13:32:25 -0800

    Updated base FT class

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,9 @@
 """Base Functional Test Class for superlists project"""
 import sys
+
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
@@ -44,3 +47,32 @@ class FunctionalTest(StaticLiveServerTestCase):
 
   def get_item_input_box(self):
     return self.browser.find_element_by_id('id_text')
+
+  def switch_to_new_window(self, text_in_title):
+    retries = 60
+    while retries > 0:
+      for handle in self.browser.window_handles:
+        self.browser.switch_to_window(handle)
+        if text_in_title in self.browser.title:
+          return
+      retries -= 1
+      time.sleep(0.5)
+    self.fail('could not find window')
+
+  def wait_for_element_with_id(self, element_id):
+    WebDriverWait(self.browser, timeout=30).until(
+      lambda b: b.find_element_by_id(element_id),
+      'Could not find element with id {}. Page text was {}'.format(
+        element_id, self.browser.find_element_by_tag_name('body').text
+      )
+    )
+
+  def wait_to_be_logged_in(self, email):
+    self.wait_for_element_with_id('id_logout')
+    navbar = self.browser.find_element_by_css_selector('.navbar')
+    self.assertIn(email, navbar.text)
+
+  def wait_to_be_logged_out(self, email):
+    self.wait_for_element_with_id('id_login')
+    navbar = self.browser.find_element_by_css_selector('.navbar')
+    self.assertNotIn(email, navbar.text)
